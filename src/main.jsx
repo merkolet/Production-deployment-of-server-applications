a2f1bc96258cb2e6a7744633a1962a2dda5c3f85
merkolet <sakriuchkov@edu.hse.ru>
2026-09-17 10:35:41 +0300

    Дз№1

---
+++ src/main.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <div style={{ fontFamily: 'sans-serif', padding: '40px', textAlign: 'center' }}>
+    <h1>React Container is running</h1>
+  </div>
+);
